3674db1ac87e3981314a88e57ce4c957e471893c
Nata-nk <137524429+Nata-nk@users.noreply.github.com>
2024-01-07 19:26:51 +0200

    Update cod.js

--- js/cod.js
+++ js/cod.js
@@ -1,8 +1,13 @@
-function calculateTotal(number) {
-  let mun = 0;
-  let result = mun;
-  while (mun < number) {
-mun += 1;
-return result += mun;   
+function calculateTotalPrice(order) {
+  let totalPrice = 0; 
+  for (const total of order) {
+     totalPrice += total;
 }
+   return totalPrice;
 }
+
+
+console.log(calculateTotalPrice([12, 85, 37, 4]))
+console.log(calculateTotalPrice([164, 48, 291]))
+console.log(calculateTotalPrice([]))
+console.log(calculateTotalPrice([412, 371, 94, 63, 176]))
